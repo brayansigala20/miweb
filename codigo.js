d7ac5fda953b774ab39573ad63d0b58543b16619
brayan sigala <brayansigala21@gmail.com>
2022-03-31 22:26:06 -0700

    actualizando codigo.js

--- codigo.js
+++ codigo.js
@@ -1,5 +1,19 @@
-const  objet = {
-    modulo: 1,
-    modulo: 2,
-    modulo: 3
-}
+const usuarios = async ()=>{
+    let peticion = await axios("https://reqres.in/api/users?page=2")
+    return peticion
+    }
+    
+    const userId =  (Id)=>{
+        return  new Promise((res,rej)=>{
+            let resultados = usuarios().then(res=>res.data.data);
+            res(resultados.then(res=>{
+                let array =[res]
+                for(const i=0; i<array.length; i++){
+                 return array
+                }}))
+                
+    })
+        }
+    
+    userId().then(res=>console.log(res))
+    
