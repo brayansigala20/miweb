eec897e317e232d77f7ebbb9e3d7bd749618fbeb
brayan sigala <brayansigala21@gmail.com>
2022-04-03 21:11:12 -0600

    actualizando codigo 1

--- codigo.js
+++ codigo.js
@@ -1,19 +1,19 @@
-const usuarios = async ()=>{
-    let peticion = await axios("https://reqres.in/api/users?page=2")
-    return peticion
-    }
-    
-    const userId =  (Id)=>{
-        return  new Promise((res,rej)=>{
-            let resultados = usuarios().then(res=>res.data.data);
-            res(resultados.then(res=>{
-                let array =[res]
-                for(const i=0; i<array.length; i++){
-                 return array
-                }}))
-                
-    })
-        }
-    
-    userId().then(res=>console.log(res))
-    
+"use strict";
+const usuariosy = async ()=>{
+ let peticion = await axios("https://reqres.in/api/users?page=2")
+ return peticion.data.data
+}
+const usrID =async()=>{
+    let usuarios =await usuariosy() 
+    let ID = prompt("id")
+    let resultado =`Nombre: ${usuarios[ID].first_name}<br>
+                    apellido: ${usuarios[ID].last_name}<br>
+                    e-mail: ${usuarios[ID].email}<br>
+                    ID usuario: ${usuarios[ID].id}`
+ let h1 =document.createElement("h1")
+ let caja = document.querySelector(".container__msj").appendChild(h1).innerHTML= resultado
+}
+
+
+ document.getElementById("boton").addEventListener("click",usrID)
+
